b54a0838a0668e5784bcf9e358257f797a6bb1c7
thisfeeling <89946683+thisfeeling@users.noreply.github.com>
2023-05-10 21:06:04 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,3 +92,18 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2021-08-01
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+TARGET_USES_64_BIT_BINDER := true
+
+# APEX
+OVERRIDE_TARGET_FLATTEN_APEX := true
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0

2782cfb30917f2d18a219ba0cd5118d20626b579
thisfeeling <89946683+thisfeeling@users.noreply.github.com>
2023-05-10 21:09:04 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-# Prebuilts
+# Kernel - prebuilt
 TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/prebuilt/Image.gz-dtb
 
 # Kernel

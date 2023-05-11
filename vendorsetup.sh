ce01c1217ee1ec172af3afc003e8b5b38ed54248
thisfeeling <89946683+thisfeeling@users.noreply.github.com>
2023-05-10 19:20:22 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 add_lunch_combo omni_ali-user
 add_lunch_combo omni_ali-userdebug
 add_lunch_combo omni_ali-eng

02ec06a5829e299965bc8b45f876515b2c087fcc
Stiven <89946683+thisfeeling@users.noreply.github.com>
2023-05-28 16:32:31 -0500

    Update to android 10.0

--- omni_ali.mk
+++ omni_ali.mk
@@ -13,19 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
+# Properties for decryption
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8953 \
+    ro.hardware.gatekeeper=msm8953 \
+    ro.hardware.bootctrl=msm8953 \
+    ro.build.system_root_image=false
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_ali
 PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := ali
 PRODUCT_MODEL := Moto G(6)
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_GMS_CLIENTID_BASE := Android-Motorola
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ali_retail-user 9 PPSS29.55-37-7-10 c25e release-keys"
 
-BUILD_FINGERPRINT := motorola/ali_retail/ali:9/PPSS29.55-37-7-10/c25e:user/release-keys
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)/device.mk)
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo omni_ali-user
-add_lunch_combo omni_ali-userdebug
-add_lunch_combo omni_ali-eng
